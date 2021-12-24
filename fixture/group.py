a87273d94430d5d43a9cca3b931145b4cd725b7c
podyganova <albina.yandulova@yandex.ru>
2021-12-25 01:48:19 +0300

    Задание_6. Добавлен тест по удалению группы. Добалена общая фикстура

--- fixture/group.py
+++ fixture/group.py
@@ -26,6 +26,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def delete_first(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select
+        wd.find_element_by_name("selected[]").click()
+        # submit
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
