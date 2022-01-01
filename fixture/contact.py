418c878a09b8127c65352d2fd7b42d59689c2ec0
podyganova <albina.yandulova@yandex.ru>
2022-01-01 18:28:19 +0300

    Задание_10. Оптимизированы переходы между страницами

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,7 +5,8 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("") and len(wd.find_elements_by_name("searchstring")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def create(self, contact):
         wd = self.app.wd
